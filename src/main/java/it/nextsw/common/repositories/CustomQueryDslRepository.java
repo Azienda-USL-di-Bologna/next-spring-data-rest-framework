08f8f82d060d74eae6bd606e67cfb31da6ff77ba
a.marcomini <devnull@localhost>
2018-06-14 17:01:57 +0200

    inserimento della gestione sulle stringhe con equals, contains, startswith

--- src/main/java/it/nextsw/common/repositories/CustomQueryDslRepository.java
+++ src/main/java/it/nextsw/common/repositories/CustomQueryDslRepository.java
@@ -4,10 +4,12 @@ import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.EntityPath;
 import com.querydsl.core.types.Path;
 import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.DatePath;
 import com.querydsl.core.types.dsl.DateTimePath;
 import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.core.types.dsl.StringPath;
+import it.nextsw.common.repositories.exceptions.InvalidFilterException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -15,6 +17,8 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
 import org.springframework.data.querydsl.binding.QuerydslBindings;
@@ -79,16 +83,79 @@ public interface CustomQueryDslRepository<E extends Object, ID extends Object, T
                 (StringPath path, Collection<? extends String> values) -> {
                     final List<? extends String> strings = new ArrayList<>(values);
                     Predicate res;
-                    if (values.isEmpty()) {
-                        res = Expressions.asBoolean(true).isTrue();
-                    } else if (values.size() == 1) {
-                        res = path.containsIgnoreCase(strings.get(0));
-                    } else {
-                        BooleanBuilder b = new BooleanBuilder();
-                        values.forEach(value -> b.or(path.containsIgnoreCase(value)));
-                        res = b;
+                    try {
+                        if (values.isEmpty()) {
+                            res = Expressions.asBoolean(true).isTrue();
+                        } else if (values.size() == 1) {
+                            res = getStringPredicate(strings.get(0), path);
+                        } else {
+                            BooleanBuilder b = new BooleanBuilder();
+                            for (String value : values) {
+                                b = b.or(getStringPredicate(value, path));
+                            }
+                            res = b;
+                        }
+                        return Optional.of(res);
+                    } catch (InvalidFilterException ex) {
+                        return Optional.of(Expressions.asBoolean(true).isTrue());
                     }
-                    return Optional.of(res);
                 });
+
     }
+
+    default StringOperation getStringOperation(String valueToParse) throws InvalidFilterException {
+        StringOperation res;
+
+        String regex = "\\$(.*?)\\((.*)\\)";
+        Pattern r = Pattern.compile(regex);
+        Matcher m = r.matcher(valueToParse);
+
+        if (m.find()) {
+            System.out.println("Found value: " + m.group(0));
+            try {
+                String operation = m.group(1);
+                String value = m.group(2);
+                res = new StringOperation(StringOperation.Operators.valueOf(operation), value);
+            } catch (Exception ex) {
+                throw new InvalidFilterException("filtro non valido", ex);
+            }
+        } else {
+            throw new InvalidFilterException("filtro non valido");
+        }
+
+        return res;
+    }
+
+    default Predicate getStringPredicate(String valueToParse, StringPath stringPath) throws InvalidFilterException {
+
+        BooleanExpression res;
+
+        StringOperation stringOperation = getStringOperation(valueToParse);
+
+        switch (stringOperation.getOperator()) {
+            case contains:
+                res = stringPath.contains(stringOperation.getValue());
+                break;
+            case containsIgnorecase:
+                res = stringPath.containsIgnoreCase(stringOperation.getValue());
+                break;
+            case startsWith:
+                res = stringPath.startsWith(stringOperation.getValue());
+                break;
+            case startsWithIgnoreCase:
+                res = stringPath.startsWithIgnoreCase(stringOperation.getValue());
+                break;
+            case equals:
+                res = stringPath.eq(stringOperation.getValue());
+                break;
+            case equalsIgnoreCase:
+                res = stringPath.equalsIgnoreCase(stringOperation.getValue());
+                break;
+            default:
+                throw new InvalidFilterException(String.format("operatore %s non valido", stringOperation.getOperator()));
+
+        }
+        return res;
+    }
+
 }
