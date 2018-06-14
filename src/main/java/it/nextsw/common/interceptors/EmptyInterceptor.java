686c3104ed04306ccba97e917f07587a0e0ef6f3
a.marcomini <devnull@localhost>
2018-06-14 15:20:45 +0200

    creazione EmptyInterceptor

---
+++ src/main/java/it/nextsw/common/interceptors/EmptyInterceptor.java
@@ -0,0 +1,43 @@
+package it.nextsw.common.interceptors;
+
+import com.querydsl.core.types.Predicate;
+import it.nextsw.common.interceptors.exceptions.RollBackInterceptorException;
+import java.util.List;
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ *
+ * @author gdm
+ */
+public abstract class EmptyInterceptor implements RestControllerInterceptor {
+
+    @Override
+    public Predicate beforeSelectQueryInterceptor(Predicate initialPredicate, Map<String, String> additionalData, HttpServletRequest request) {
+        return initialPredicate;
+    }
+
+    @Override
+    public List<Object> afterSelectQueryInterceptor(List<Object> entities, Map<String, String> additionalData, HttpServletRequest request) {
+        return entities;
+    }
+
+    @Override
+    public Object afterSelectQueryInterceptor(Object entity, Map<String, String> additionalData, HttpServletRequest request) {
+        return entity;
+    }
+
+    @Override
+    public Object beforeCreateInterceptor(Object entity, Map<String, String> additionalData, HttpServletRequest request) throws RollBackInterceptorException {
+        return entity;
+    }
+
+    @Override
+    public Object beforeUpdateInterceptor(Object entity, Map<String, String> additionalData, HttpServletRequest request) throws RollBackInterceptorException {
+        return entity;
+    }
+
+    @Override
+    public void beforeDeleteInterceptor(Object entity, Map<String, String> additionalData, HttpServletRequest request) throws RollBackInterceptorException {
+    }
+}
