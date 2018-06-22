453fbf6330dd0c2c49c03ea0c2a2e706f3070353
g.zoli <devnull@localhost>
2018-06-22 10:27:34 +0200

    gestito caso di parma (path doppio nell'header dell'azienda)

--- src/main/java/it/nextsw/common/utils/CommonUtils.java
+++ src/main/java/it/nextsw/common/utils/CommonUtils.java
@@ -1,6 +1,8 @@
 package it.nextsw.common.utils;
 
 import javax.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
@@ -10,8 +12,19 @@ import org.springframework.util.StringUtils;
  */
 @Component
 public class CommonUtils {
-    
+
+    private static final Logger log = LoggerFactory.getLogger(CommonUtils.class);
+
     public String getHostname(HttpServletRequest request) {
-        return StringUtils.hasText(request.getHeader("X-Forwarded-Host"))? request.getHeader("X-Forwarded-Host"): request.getServerName();
+
+        // TODO: non è detto che vada bene tornare sempre il primo elemento, bisognerebbe controllare che il Path dell'azienda matchi con uno qualsiasi degli elementi
+        String header = request.getHeader("X-Forwarded-Host");
+        if (StringUtils.hasText(header)) {
+            String[] headerToken = header.split(",");
+            return headerToken[0];
+        } else {
+            return request.getServerName();
+        }
+
     }
 }
