1df658acdb2a3196c3f51230f9582825e00817f0
g.demarco <devnull@localhost>
2020-05-12 10:38:22 +0200

    chiamata al super()

--- src/main/java/it/nextsw/common/dialect/CustomPostgresDialect.java
+++ src/main/java/it/nextsw/common/dialect/CustomPostgresDialect.java
@@ -5,7 +5,8 @@ import org.hibernate.dialect.PostgreSQL94Dialect;
 public class CustomPostgresDialect extends PostgreSQL94Dialect {
 
     public CustomPostgresDialect() {
-        registerFunction("fts_match", new PostgreSQLFullTextSearchFunction());
+        super();
+        this.registerFunction("fts_match", new PostgreSQLFullTextSearchFunction());
     }
 
 }
