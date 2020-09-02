b84c7415f2d0ec050f45e1b550f893bf8131f4bb
g.demarco <devnull@localhost>
2020-09-02 14:51:53 +0200

    inserita operazione hibernate bitand

--- src/main/java/it/nextsw/common/dialect/CustomPostgresDialect.java
+++ src/main/java/it/nextsw/common/dialect/CustomPostgresDialect.java
@@ -1,6 +1,8 @@
 package it.nextsw.common.dialect;
 
 import org.hibernate.dialect.PostgreSQL94Dialect;
+import org.hibernate.dialect.function.SQLFunctionTemplate;
+import org.hibernate.type.IntegerType;
 
 public class CustomPostgresDialect extends PostgreSQL94Dialect {
 
@@ -8,6 +10,7 @@ public class CustomPostgresDialect extends PostgreSQL94Dialect {
         super();
         this.registerFunction("fts_match", new PostgreSQLFullTextSearchFunction());
         this.registerFunction("array_operation", new PostgresArrayFunctions());
+        this.registerFunction("bitand", new SQLFunctionTemplate(IntegerType.INSTANCE, "(?1 & ?2)"));
     }
 
 }
