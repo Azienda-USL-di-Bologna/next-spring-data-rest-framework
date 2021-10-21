f96687d6542f189e6a22cf13b796839f25b29909
g.demarco <devnull@localhost>
2021-10-21 11:19:14 +0200

    gestione migliore eccezioni

--- src/main/java/it/nextsw/common/controller/BeforeUpdateEntityApplier.java
+++ src/main/java/it/nextsw/common/controller/BeforeUpdateEntityApplier.java
@@ -1,8 +1,11 @@
 package it.nextsw.common.controller;
 
+import it.nextsw.common.controller.exceptions.BeforeUpdateEntityApplierException;
 import it.nextsw.common.utils.EntityReflectionUtils;
 import java.lang.reflect.InvocationTargetException;
 import java.util.function.Consumer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import org.springframework.stereotype.Component;
@@ -26,10 +29,18 @@ public class BeforeUpdateEntityApplier {
 //    }
     
     @Transactional(propagation = Propagation.REQUIRES_NEW, readOnly = true)
-    public void beforeUpdateApply(Consumer<Object> fn) throws NoSuchMethodException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+    public void beforeUpdateApply(Consumer<Object> fn) throws BeforeUpdateEntityApplierException {
 //        Object currentEntityObj = this.currentEntity.get();
-        Object entityBeforeModify = entityManager.find(currentEntity.get().getClass(), EntityReflectionUtils.getPrimaryKeyValue(currentEntity.get()));
-        fn.accept(entityBeforeModify);
+        Object entityBeforeModify;
+        try {
+            entityBeforeModify = entityManager.find(currentEntity.get().getClass(), EntityReflectionUtils.getPrimaryKeyValue(currentEntity.get()));
+            fn.accept(entityBeforeModify);
+        } catch (NoSuchMethodException | IllegalAccessException | IllegalArgumentException | InvocationTargetException ex) {
+            throw new BeforeUpdateEntityApplierException("errore nel reperire l'entità prima delle modifiche dal database", ex);
+        } catch (Exception ex) {
+            throw new BeforeUpdateEntityApplierException("errore nel BeforeUpdateEntityApplier", ex);
+        }
+        
     }
 
     public void setCurrentEntity(Object currentEntity) {
