241a70ed9deed7b2b01837eed7e5054363648d12
D'Onza Michele <m.donza@nextsw.it>
2024-08-08 11:05:18 +0200

    no message

--- framework-core/src/main/java/it/nextsw/common/dialect/PostgreSQLFullTextSearchFunction.java
+++ framework-core/src/main/java/it/nextsw/common/dialect/PostgreSQLFullTextSearchFunction.java
@@ -4,6 +4,15 @@ import java.util.List;
 import org.hibernate.QueryException;
 import org.hibernate.engine.spi.Mapping;
 import org.hibernate.engine.spi.SessionFactoryImplementor;
+import org.hibernate.query.ReturnableType;
+import org.hibernate.query.spi.QueryEngine;
+import org.hibernate.query.sqm.function.FunctionKind;
+import org.hibernate.query.sqm.function.SelfRenderingSqmFunction;
+import org.hibernate.query.sqm.function.SqmFunctionDescriptor;
+import org.hibernate.query.sqm.produce.function.ArgumentsValidator;
+import org.hibernate.query.sqm.tree.SqmTypedNode;
+import org.hibernate.query.sqm.tree.predicate.SqmPredicate;
+import org.hibernate.query.sqm.tree.select.SqmOrderByClause;
 import org.hibernate.type.BasicTypeReference;
 import org.hibernate.type.StandardBasicTypes;
 import org.hibernate.type.Type;
@@ -12,7 +21,7 @@ import org.hibernate.type.Type;
  *
  * @author gdm
  */
-public class PostgreSQLFullTextSearchFunction implements SQLFunction {
+public class PostgreSQLFullTextSearchFunction implements SqmFunctionDescriptor {
 
     public PostgreSQLFullTextSearchFunction() {
     }
@@ -68,6 +77,56 @@ public class PostgreSQLFullTextSearchFunction implements SQLFunction {
         }
         String tsCondition = " to_tsquery ('" + ftsConfig + "',$$" + value + ":*$$) @@ " + field;
         return tsCondition;
+    }
+
+    @Override
+    public <T> SelfRenderingSqmFunction<T> generateSqmExpression(List<? extends SqmTypedNode<?>> arguments, ReturnableType<T> impliedResultType, QueryEngine queryEngine) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public <T> SelfRenderingSqmFunction<T> generateAggregateSqmExpression(List<? extends SqmTypedNode<?>> arguments, SqmPredicate filter, ReturnableType<T> impliedResultType, QueryEngine queryEngine) {
+        return SqmFunctionDescriptor.super.generateAggregateSqmExpression(arguments, filter, impliedResultType, queryEngine); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+
+    @Override
+    public <T> SelfRenderingSqmFunction<T> generateOrderedSetAggregateSqmExpression(List<? extends SqmTypedNode<?>> arguments, SqmPredicate filter, SqmOrderByClause withinGroupClause, ReturnableType<T> impliedResultType, QueryEngine queryEngine) {
+        return SqmFunctionDescriptor.super.generateOrderedSetAggregateSqmExpression(arguments, filter, withinGroupClause, impliedResultType, queryEngine); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+
+    @Override
+    public <T> SelfRenderingSqmFunction<T> generateWindowSqmExpression(List<? extends SqmTypedNode<?>> arguments, SqmPredicate filter, Boolean respectNulls, Boolean fromFirst, ReturnableType<T> impliedResultType, QueryEngine queryEngine) {
+        return SqmFunctionDescriptor.super.generateWindowSqmExpression(arguments, filter, respectNulls, fromFirst, impliedResultType, queryEngine); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+
+    @Override
+    public <T> SelfRenderingSqmFunction<T> generateSqmExpression(SqmTypedNode<?> argument, ReturnableType<T> impliedResultType, QueryEngine queryEngine) {
+        return SqmFunctionDescriptor.super.generateSqmExpression(argument, impliedResultType, queryEngine); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+
+    @Override
+    public <T> SelfRenderingSqmFunction<T> generateSqmExpression(ReturnableType<T> impliedResultType, QueryEngine queryEngine) {
+        return SqmFunctionDescriptor.super.generateSqmExpression(impliedResultType, queryEngine); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+
+    @Override
+    public boolean alwaysIncludesParentheses() {
+        return SqmFunctionDescriptor.super.alwaysIncludesParentheses(); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+
+    @Override
+    public String getSignature(String name) {
+        return SqmFunctionDescriptor.super.getSignature(name); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+
+    @Override
+    public FunctionKind getFunctionKind() {
+        return SqmFunctionDescriptor.super.getFunctionKind(); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+
+    @Override
+    public ArgumentsValidator getArgumentsValidator() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
     
 }
